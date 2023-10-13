710b634bf903dab69f70a9b2bb40b94ca9e23b2d
bb420f@gmail.com <bb420f@gmail.com>
2023-10-13 16:07:17 +0500

    ab or changes laye ga wo ik

--- js/script.js
+++ js/script.js
@@ -37,4 +37,94 @@ $(document).ready(function () {
         $headerResult.slideUp(); // Slide up .header_serach_result when input loses focus
     });
   
+
+
+    const alphabet = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+  
+    const mainAlpha = $(".alphabet");
+    for (let index = 0; index < alphabet.length; index++) {
+      $(mainAlpha).append(
+        `<a href="" class="alphabet-item">${alphabet[index]}</a>`
+      );
+    }
+    $(".alphabet-item").first().addClass("active");
+  
+    $(".alphabet-item").click(function () {
+      $(this).addClass("active");
+      $(this).siblings().removeClass("active");
+    });
+
+
+    $(".sidebar_main_topics ul li").click(function () {
+      $(this).addClass("active");
+      $(this).siblings().removeClass("active");
+    });
+  
+
+    var swiper = new Swiper(".latest_card_main_wrapper", {
+      slidesPerView: 1,
+      effect: "fade",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        dynamicBullets: true,
+      },
+    });
+
+   
+var swiper_thumb = new Swiper(".hero_thumbnail_slider", {
+  spaceBetween: 10,
+  slidesPerView: "auto",
+  freeMode: true,
+  watchSlidesProgress: true,
+  autoplay: {
+    delay: 5000, // Adjust the delay (in milliseconds) as needed
+  },
+  speed: 800,
+});
+
+var swiper2 = new Swiper(".hero_swiper_main", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+  thumbs: {
+    swiper: swiper_thumb,
+  },
+  autoplay: {
+    delay: 5000, // Adjust the delay (in milliseconds) as needed
+  },
+  speed: 800,
+});
 });
